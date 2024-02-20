4c068522c47c3d96c23aca26b4d823d65cc10ac9
TFGYouKno <brockhoffharrison@comcast.net>
2024-02-19 20:49:35 -0500

    first

---
+++ python/pw_demo.py
@@ -0,0 +1,26 @@
+print("welcome to CT Rhett!")
+name = "rhett"
+print("welcome to CT " + name)
+
+print(f"welcome to ct {name}!")
+
+
+def namer(f_name):
+    print(f"WELCOME WELCOME WELCOME CT {f_name}")
+    print("This will run too!")
+    return 3
+#namer("Jenequa")
+#namer("Zack")
+#namer("Rhett")
+#namer("Harrison")
+#namer("Buhay")
+#print(namer("Adrian") + namer("Bryce"))
+print(namer("adrian"))
+#print(namer("Adrian") == 3)
+#name2 = "Adrian"
+
+#def adder(num1, num2):
+#    return num1 + num2
+
+#print(adder(5, 6))
+
